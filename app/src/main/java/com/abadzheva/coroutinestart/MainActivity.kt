e2e536d403e5e5e33cbeb5ad7a165470ed5f311c
k8abadzheva <make.me.loco@gmail.com>
2025-02-25 22:55:22 +0300

    refactor: replace sequential coroutines with async/await
    - Replaces `launch` with `async` to enable concurrent execution of `loadCity` and `loadTemperature`.
    - Uses `await` to retrieve the results of asynchronous operations.
    - Displays city and temperature in a Toast after both asynchronous operations are completed.

--- app/src/main/java/com/abadzheva/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/abadzheva/coroutinestart/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import com.abadzheva.coroutinestart.databinding.ActivityMainBinding
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -25,19 +26,27 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.setOnClickListener {
             binding.progress.isVisible = true
             binding.buttonLoad.isEnabled = false
-            val jobCity =
-                lifecycleScope.launch {
+            val deferredCity =
+                lifecycleScope.async {
                     val city = loadCity()
                     binding.tvLocation.text = city
+                    city
                 }
-            val jobTemperature =
-                lifecycleScope.launch {
+            val deferredTemperature =
+                lifecycleScope.async {
                     val temp = loadTemperature()
                     binding.tvTemperature.text = "$temp"
+                    temp
                 }
             lifecycleScope.launch {
-                jobCity.join()
-                jobTemperature.join()
+                val city = deferredCity.await()
+                val temp = deferredTemperature.await()
+                Toast
+                    .makeText(
+                        this@MainActivity,
+                        "City: $city Temp: $temp",
+                        Toast.LENGTH_SHORT,
+                    ).show()
                 binding.progress.isVisible = false
                 binding.buttonLoad.isEnabled = true
             }
